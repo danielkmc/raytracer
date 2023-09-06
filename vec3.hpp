399501f1c07ad120ee3074ddf967ac06e02e33bf
Daniel Chen <danielmc4102@gmail.com>
2023-09-06 15:43:43 -0500

    Added camera and viewpoint

--- vec3.hpp
+++ vec3.hpp
@@ -14,7 +14,7 @@ public:
     vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
 
     double x() const { return e[0]; }
-    double y() const { return e[0]; }
+    double y() const { return e[1]; }
     double z() const { return e[2]; }
 
     vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); }
@@ -75,7 +75,7 @@ inline vec3 operator/(const vec3& v, double t) {
 }
 
 inline double dot(const vec3& u, const vec3& v) {
-    return u.e[0]*v.e[1] + 
+    return u.e[0]*v.e[0] + 
            u.e[1]*v.e[1] + 
            u.e[2]*v.e[2];
 }
